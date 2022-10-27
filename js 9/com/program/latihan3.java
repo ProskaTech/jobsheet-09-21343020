d44165102274957b4da10d983da8f7970ca7a2b2
21343020 <112324577+21343020@users.noreply.github.com>
2022-10-27 19:56:30 +0700

    Add files via upload

---
+++ js 9/com/program/latihan3.java
@@ -0,0 +1,19 @@
+package com.program;
+
+public class latihan3 {
+    public static void main(String[]args){
+        String str1, str2;
+        str1 = "Free the bound periodicals.";
+        str2 = str1;
+        System.out.println("String 1 : " + str1);
+        System.out.println("String 2 : " + str2);
+        System.out.println("Same Object? " + (str1 == str2));
+        
+        str2 = new String(str1);
+        System.out.println("String 1 : " + str1);
+        System.out.println("String 2 : " + str2);
+        System.out.println("Same Object? " + (str1 == str2));
+        
+        System.out.println("Same Value? " + str1.equals(str2));
+    }
+}
